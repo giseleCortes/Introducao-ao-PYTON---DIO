46981e77e6d8c5ba065a8bd8c9a4e12bb060e3d9
giseleCortes <86819936+giseleCortes@users.noreply.github.com>
2022-04-27 21:04:20 -0300

    elif

--- aula3 IF ELIF ELSE INPUT.py
+++ aula3 IF ELIF ELSE INPUT.py
@@ -4,7 +4,7 @@ c = int(input('Terceiro valor: '))
 
 if a > b and a > c:
     print('O maior numero é: {}'.format(a))
-elif b > a and b > c: #
+elif b > a and b > c: #elif é igual a else+if
     print('O maior numero é: {}'.format(b))
 else:
     print('O maior numero é: {}'.format(c))
